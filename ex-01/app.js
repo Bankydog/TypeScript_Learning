bf1db635321dbe7c8d917548cc0d5e480b344fe5
Bank <blankinfear@gmail.com>
2025-02-17 15:38:17 +0700

    learn about unknown and never

--- ex-01/app.js
+++ ex-01/app.js
@@ -1,17 +1,11 @@
-function combine(n1, n2, resultConversion) {
-    var result;
-    if ((typeof n1 === "number" && typeof n2 === "number") ||
-        resultConversion === "as-number") {
-        result = +n1 + +n2;
-    }
-    else {
-        result = n1.toString() + n2.toString();
-    }
-    return result;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Bank";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedNames = combine("Bank", "Bow", "as-text");
-console.log(combinedNames);
-var combinedStringAges = combine("10", "226", "as-number");
-console.log(combinedStringAges);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occurred!", 500);
